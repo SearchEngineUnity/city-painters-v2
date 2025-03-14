36cfbb0c06e9e2687c7ed04f92479f7055b3d6e9
sc-searchengineunity <sharon@searchengeineunity.com>
2025-03-14 11:48:26 -0400

    update contact us tag color to cp brand

--- packages/web/src/components/navs/footerElements/ContactUsTag.js
+++ packages/web/src/components/navs/footerElements/ContactUsTag.js
@@ -18,9 +18,7 @@ const Inner = styled.div`
   text-align: center;
   width: 120px;
   height: 42px;
-  background-color: #406ccb;
-  // background: rgb(169, 169, 169);
-  // background: rgba(200, 0, 0, 0.5);
+  background-color: #8bc44a;
   margin-top: 60px;
   margin-right: -42px;
   padding-top: 5px;
